6f8fbfff74f8fa4ae3f908bf17662fdbe6e8d084
ToppleKek <braebear85@gmail.com>
2021-03-19 20:31:46 -0400

    command utils: fix formatting

--- src/utils/command_utils.js
+++ src/utils/command_utils.js
@@ -35,69 +35,69 @@ module.exports = {
     },
 
     execute_command(bot, msg, command, tokens) {
-    // Merge strings
-    // [num, str, str, str, mention] -> [num, merged_string, mention]
-    let buf = "";
-    for (let i = 0; i < tokens.length; ++i) {
-        if (tokens[i].type === 'string') {
-            let j = i;
-            while (tokens[i] && tokens[i].type === 'string')
-                buf += new String(tokens[i++].value) + ' ';
-
-            buf = buf.trimEnd();
-
-            tokens[j] = { type: 'string', value: buf };
-            tokens.splice(j + 1, i - j - 1);
-        }
-    }
-
-    bot.logger.debug(`check_command: parsed args to: ${util.inspect(tokens)}\nargs needed: ${util.inspect(command.args_list)}`);
-
-    const args = new Map();
-    const optional_args = new Map();
-
-    if (command.args_list.position_independent) {
+        // Merge strings
+        // [num, str, str, str, mention] -> [num, merged_string, mention]
+        let buf = "";
         for (let i = 0; i < tokens.length; ++i) {
-            for (let j = 0; j < command.args_list.args.length; ++j) {
-                if (token[i].type === 'string' && command.args_list.args[j].types.includes('string')) {
-                    args.set(command.args_list.args[i].name, { type: 'string', value: module.exports.join_token_string(i, tokens) });
-                    break;
-                } else if (command.args_list.args[j].types.includes(tokens[i].type)) {
-                    args.set(command.args_list.args[j].name, tokens[i]);
-                    break;
-                }
+            if (tokens[i].type === 'string') {
+                let j = i;
+                while (tokens[i] && tokens[i].type === 'string')
+                    buf += new String(tokens[i++].value) + ' ';
+
+                buf = buf.trimEnd();
+
+                tokens[j] = { type: 'string', value: buf };
+                tokens.splice(j + 1, i - j - 1);
             }
         }
-    } else {
-        let i;
-        for (i = 0; i < tokens.length; ++i) {
-            if (command.args_list.args.length <= i)
-                break;
-            
-            // If there is only one argument required and it is a string, then just turn the whole message into a string.
-            if (command.args_list.args[i].types.length === 1 && command.args_list.args[i].types[0] === 'string')
-                args.set(command.args_list.args[i].name, {type: 'string', value: module.exports.join_token_string(i, tokens) });
-            else if (command.args_list.args[i].types.includes(tokens[i].type))
-                args.set(command.args_list.args[i].name, tokens[i]);
-            else
-                throw new CommandError('SyntaxError', `Expected type: ${command.args_list.args[i].types.join(' or ')} ` + 
-                            `for argument: ${command.args_list.args[i].name} `  +
-                            `but got: ${tokens[i].value} (${tokens[i].type}) instead`);
+
+        bot.logger.debug(`check_command: parsed args to: ${util.inspect(tokens)}\nargs needed: ${util.inspect(command.args_list)}`);
+
+        const args = new Map();
+        const optional_args = new Map();
+
+        if (command.args_list.position_independent) {
+            for (let i = 0; i < tokens.length; ++i) {
+                for (let j = 0; j < command.args_list.args.length; ++j) {
+                    if (token[i].type === 'string' && command.args_list.args[j].types.includes('string')) {
+                        args.set(command.args_list.args[i].name, { type: 'string', value: module.exports.join_token_string(i, tokens) });
+                        break;
+                    } else if (command.args_list.args[j].types.includes(tokens[i].type)) {
+                        args.set(command.args_list.args[j].name, tokens[i]);
+                        break;
+                    }
+                }
+            }
+        } else {
+            let i;
+            for (i = 0; i < tokens.length; ++i) {
+                if (command.args_list.args.length <= i)
+                    break;
+                
+                // If there is only one argument required and it is a string, then just turn the whole message into a string.
+                if (command.args_list.args[i].types.length === 1 && command.args_list.args[i].types[0] === 'string')
+                    args.set(command.args_list.args[i].name, {type: 'string', value: module.exports.join_token_string(i, tokens) });
+                else if (command.args_list.args[i].types.includes(tokens[i].type))
+                    args.set(command.args_list.args[i].name, tokens[i]);
+                else
+                    throw new CommandError('SyntaxError', `Expected type: ${command.args_list.args[i].types.join(' or ')} ` + 
+                                `for argument: ${command.args_list.args[i].name} `  +
+                                `but got: ${tokens[i].value} (${tokens[i].type}) instead`);
+            }
+
+            for (let j = 0; i < tokens.length; ++i, ++j) {
+                if (command.args_list.optional_args.length <= i)
+                    break;
+                if (command.args_list.optional_args[j].types.length === 1 && command.args_list.optional_args[j].types[0] === 'string')
+                    optional_args.set(command.args_list.optional_args[j].name, {type: 'string', value: module.exports.join_token_string(i, tokens) });
+                else if (command.args_list.optional_args[j].types.includes(tokens[i].type))
+                    optional_args.set(command.args_list.optional_args[j].name, tokens[i]);
+            }
         }
 
-        for (let j = 0; i < tokens.length; ++i, ++j) {
-            if (command.args_list.optional_args.length <= i)
-                break;
-            if (command.args_list.optional_args[j].types.length === 1 && command.args_list.optional_args[j].types[0] === 'string')
-                optional_args.set(command.args_list.optional_args[j].name, {type: 'string', value: module.exports.join_token_string(i, tokens) });
-            else if (command.args_list.optional_args[j].types.includes(tokens[i].type))
-                optional_args.set(command.args_list.optional_args[j].name, tokens[i]);
-        }
-    }
+        if (args.size !== command.args_list.args.length)
+            throw new CommandError('Argument Error', `Expected ${command.args_list.args.length} args but ${args.size} were provided`);
 
-    if (args.size !== command.args_list.args.length)
-        throw new CommandError('Argument Error', `Expected ${command.args_list.args.length} args but ${args.size} were provided`);
-
-    command.main(bot, new Map([...args, ...optional_args]), msg);
+        command.main(bot, new Map([...args, ...optional_args]), msg);
     }
 }

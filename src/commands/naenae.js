9304d91cb88bf414e97c0fa481e291d106ba174c
ToppleKek <topkek@topplekek.xyz>
2022-08-25 19:43:09 -0400

    naenae: new command to ban members

---
+++ src/commands/naenae.js
@@ -0,0 +1,72 @@
+const { Message } = require('discord.js');
+const Bot = require('../bot');
+const util = require('util');
+const CommandError = require('../command_error');
+
+module.exports.help = 'Ban a member from the guild';
+module.exports.usage = '#PREFIXnaenae <target> <?days> <?reason>';
+module.exports.required_permissions = ['BAN_MEMBERS'];
+module.exports.args_list = {
+    position_independent: false,
+    args: [{
+        name: 'target',
+        type: 'user',
+        description: 'The user to ban'
+    }],
+    optional_args: [{
+        name: 'days',
+        type: 'number',
+        description: 'The number of days worth of messages to delete (default=7)'
+    }, {
+        name: 'reason',
+        type: 'string',
+        description: 'A reason for the ban'
+    }]
+};
+
+/**
+ * @param {Bot} bot Bot object that called
+ * @param {Map} args Map of arguments
+ * @param {Message} msg Message Object
+ */
+module.exports.main = async (bot, args, msg) => {
+    console.dir(args);
+    const member = msg.guild.members.resolve(args.get('target'));
+
+    if (!member)
+        throw new CommandError('ArgumentError', 'User not in guild');
+
+    if (!member.bannable)
+        throw new CommandError('BotPermissionError', 'This user is not bannable by the bot');
+
+    if (member.roles.highest.rawPosition >= msg.guild.members.resolve(msg.author).roles.highest.rawPosition)
+        throw new CommandError('PermissionError', `${member.user.tag} has a higher or equal role`);
+
+    const opts = {
+        days: args.get('days') || 7,
+        reason: `Ban issued by: ${msg.author.tag} - ` + args.get('reason') || `(no reason provided)`
+    };
+
+    const ban_embed = {
+        color: 1571692,
+        title: `${member.user.tag} JUST GOT NAENAED`,
+        description: 'GET FRICKED KIDDO',
+        thumbnail: {
+            url: 'https://topplekek.xyz/lmao.gif',
+        },
+        timestamp: new Date(),
+    };
+
+    const dm_embed = {
+        color: 1571692,
+        title: `Get heckin naenaed from ${msg.guild.name}`,
+        description: `They banned you for \`${args.get('reason')}\``,
+        timestamp: new Date(),
+    };
+
+    await member.createDM().then((dm_channel) => dm_channel.send({embeds:[dm_embed]}));
+
+    member.ban(opts)
+        .then(() => msg.channel.send({embeds: [ban_embed]}))
+        .catch((err) => msg.respond_error(`API Error: \`\`\`${err}\`\`\``));
+};
